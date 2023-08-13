adb5e13d2662c6d66d5dd342f63d63472eefcde5
Yusuf Khasbulatov <khashashin@gmail.com>
2023-08-13 07:23:04 +0200

    feat(setup.py): add custom build and publish commands
    - Introduced `build` command for packaging the project.
    - Added `publish` command with version option to streamline the PyPI upload process.

--- setup.py
+++ setup.py
@@ -1,8 +1,40 @@
-from setuptools import setup, find_packages
+import os
+from setuptools import setup, find_packages, Command
 
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
+
+
+class BuildCommand(Command):
+    description = 'Build the package'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system('python setup.py sdist')
+
+
+class PublishCommand(Command):
+    description = 'Publish the package to PyPI'
+    user_options = [('version=', 'v', 'version number of the package to be uploaded')]
+    version = None
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        if self.version is None:
+            raise Exception("Version number is required. Use --version=<version_number>")
+
+    def run(self):
+        os.system(f"twine upload ./dist/django-appwrite-{self.version}.tar.gz")
+
 
 setup(
     name='django-appwrite',
@@ -37,4 +69,8 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10"
     ],
+    cmdclass={
+        'build': BuildCommand,
+        'publish': PublishCommand,
+    },
 )
